2b88d24437cf3523bc141fdafb9a02680fde68ef
wenqian <106291761+yu-wenqian@users.noreply.github.com>
2022-11-30 10:37:45 +0800

    Update projected_attack.py
    linbp+ila 
    注意：1、linbp方法暂只能用于resnet50，不能和SGM方法同时使用（因为在跳跃连接处的处理方法不同）,也不能和IR方法一起用
    2、ila方法暂时只能和linbp方法一起用，并且niters和ila_iters尽量保持一致

--- codes/basic_functions/ouradvertorch/attacks/projected_attack.py
+++ codes/basic_functions/ouradvertorch/attacks/projected_attack.py
@@ -15,7 +15,12 @@ from PIL import Image
 from ..utils import clamp, normalize_by_pnorm, rand_init_delta
 from .interaction_loss import (InteractionLoss, get_features,
                                sample_for_interaction)
+from codes.utils.util_linbp import linbp_forw_resnet50, linbp_backw_resnet50,ila_forw_resnet50,ILAProjLoss
 
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
 
 def gkern(kernlen=21, nsig=3):
     """Returns a 2D Gaussian kernel array."""
@@ -72,10 +77,14 @@ def input_diversity(input_tensor,image_width,image_resize,prob):
 class ProjectionAttacker(object):
 
     def __init__(self,
+                 attack_method,
                  model,
                  epsilon,
                  num_steps,
                  step_size,
+                 linbp_layer,
+                 ila_layer,
+                 ila_niters,
                  ord='inf',
                  image_width=224,
                  loss_fn=None,
@@ -91,10 +100,14 @@ class ProjectionAttacker(object):
                  image_resize = 255,
                  prob = 0.0,
                  rand_init=True):
+        self.attack_method = attack_method
         self.model = model
         self.epsilon = epsilon
         self.num_steps = num_steps
         self.step_size = step_size
+        self.linbp_layer = linbp_layer
+        self.ila_layer = ila_layer
+        self.ila_niters = ila_niters
         self.image_width = image_width
         self.momentum = momentum
         self.targeted = targeted
@@ -122,6 +135,7 @@ class ProjectionAttacker(object):
         :param y: a Long Tensor 1 int64
         :return:
         """
+        print(self.model)
         loss_record = {'loss1': [], 'loss2': [], 'loss': []}
         delta = torch.zeros_like(X)
         if self.rand_init and self.lam == 0:
@@ -147,12 +161,13 @@ class ProjectionAttacker(object):
             # # DI2 attack
             X_prev = X + delta
             X_DIM = input_diversity(X_prev, self.image_width, self.image_resize, self.prob)
+
             if self.m >= 1:  # Variance-reduced attack; https://arxiv.org/abs/1802.09707
-                noise_shape = list(X.shape)
+                noise_shape = list(X_DIM.shape)
                 noise_shape[0] = self.m
                 noise = noise_distribution.sample(noise_shape).squeeze() / 255
-                noise = noise.to(X.device)
-                outputs = self.model(X_DIM + delta + noise)
+                noise = noise.to(X_DIM.device)
+                outputs = self.model(X_DIM + noise)
 
                 loss1 = self.loss_fn(outputs, y.expand(self.m))
             else:
@@ -206,7 +221,7 @@ class ProjectionAttacker(object):
             # MI Attack; https://arxiv.org/abs/1710.06081
             cur_grad = normalize_by_pnorm(cur_grad, p=1)
             grad = self.momentum * grad + cur_grad
-            
+
             if self.ord == np.inf:
                 delta.data += self.step_size * grad.sign()
                 delta.data = clamp(delta.data, -self.epsilon, self.epsilon)
@@ -224,4 +239,151 @@ class ProjectionAttacker(object):
 
             delta.grad.data.zero_()
         rval = X.data + deltas
+        return rval, loss_record
+
+    def perturb_linbp_ila(self, X, y):
+        """
+        :param X_nat: a Float Tensor  1,c,h,w  float32
+        :param y: a Long Tensor 1 int64
+        :return:
+        """
+        #
+        # model = self.model
+        # model.eval()
+        # model = nn.Sequential(
+        #     Normalize(),
+        #     model
+        # )
+        # model.to(device)
+        loss_record = {'loss1': [], 'loss2': [], 'loss': []}
+
+        delta = torch.zeros_like(X)
+        if self.rand_init and self.lam == 0:
+            rand_init_delta(delta, X, self.ord, self.epsilon, 0.0, 1.0)
+            delta.data = clamp(X + delta.data, min=0.0, max=1.0) - X
+
+        X_adv = X + delta
+        X_adv.requires_grad_()
+
+        grad = torch.zeros_like(X)
+        advs = torch.zeros_like(X).repeat(self.num_steps, 1, 1, 1)
+        advs_ila = torch.zeros_like(X).repeat(self.ila_niters, 1, 1, 1)
+
+        noise_distribution = torch.distributions.normal.Normal(
+                    torch.tensor([0.0]),
+                    torch.tensor([self.sigma]).float())
+        # X_prev = X
+
+        # # DIM attack
+        # if self.prob >0:
+        #     X = input_diversity(X, self.image_width, self.image_resize, self.prob)
+
+        for i in range(self.num_steps):
+            # # DI2 attack
+            X_DIM = input_diversity(X_adv, self.image_width, self.image_resize, self.prob)
+            X_DIM.requires_grad_()
+            if self.m >= 1:  # Variance-reduced attack; https://arxiv.org/abs/1802.09707
+                noise_shape = list(X_DIM.shape)
+                noise_shape[0] = self.m
+                noise = noise_distribution.sample(noise_shape).squeeze() / 255
+                noise = noise.to(X_DIM.device)
+                X_DIM = X_DIM + noise
+
+            if 'linbp' in self.attack_method:
+                att_out, ori_mask_ls, conv_out_ls, relu_out_ls, conv_input_ls = linbp_forw_resnet50(self.model, X_DIM, True,
+                                                                                                    self.linbp_layer)
+                pred = torch.argmax(att_out, dim=1).view(-1)
+                loss1 = nn.CrossEntropyLoss()(att_out, y)
+                self.model.zero_grad()
+                cur_grad = linbp_backw_resnet50(X_adv, loss1, conv_out_ls, ori_mask_ls, relu_out_ls, conv_input_ls,
+                                                  xp=1.)
+            else:
+                att_out = self.model(X_DIM)
+                # pred = torch.argmax(att_out, dim=1).view(-1)
+                loss1 = nn.CrossEntropyLoss()(att_out, y)
+                self.model.zero_grad()
+                loss1.backward()
+                cur_grad = X_adv.grad.data
+            self.model.zero_grad()
+
+            advs[i, :, :, :] = X_adv.data
+
+            # cur_grad = delta.grad.data
+            if self.ti_size > 1:  # TI Attack; https://arxiv.org/abs/1904.02884
+                self.ti_conv.to(X.device)
+                cur_grad = self.ti_conv(cur_grad)
+
+            # MI Attack; https://arxiv.org/abs/1710.06081
+            cur_grad = normalize_by_pnorm(cur_grad, p=1)
+            grad = self.momentum * grad + cur_grad
+
+            if self.ord == np.inf:
+                X_adv.data += self.step_size * grad.sign()
+                X_adv.data = clamp(X_adv.data, X-self.epsilon, X+self.epsilon)
+                X_adv.data = clamp(X_adv, 0.0, 1.0)
+            elif self.ord == 2:
+                X_adv.data += self.step_size * normalize_by_pnorm(grad, p=2)
+                X_adv.data *= clamp(
+                    (self.epsilon * normalize_by_pnorm(X_adv.data, p=2) /
+                     X_adv.data),
+                    max=1.)
+                X_adv.data = clamp(X_adv.data, 0.0, 1.0)
+            else:
+                error = "Only ord = inf and ord = 2 have been implemented"
+                raise NotImplementedError(error)
+
+        rval = advs
+
+        if 'ila' in self.attack_method:
+            attack_img = X_adv.clone()
+            X_adv = X.clone().to(device)
+            with torch.no_grad():
+                mid_output = ila_forw_resnet50(self.model, X, self.ila_layer)
+                mid_original = torch.zeros(mid_output.size()).to(device)
+                mid_original.copy_(mid_output)
+                mid_output = ila_forw_resnet50(self.model, attack_img, self.ila_layer)
+                mid_attack_original = torch.zeros(mid_output.size()).to(device)
+                mid_attack_original.copy_(mid_output)
+            for _ in range(self.ila_niters):
+                X_adv.requires_grad_(True)
+                mid_output = ila_forw_resnet50(self.model, X_adv, self.ila_layer)
+                loss = ILAProjLoss()(
+                    mid_attack_original.detach(), mid_output, mid_original.detach(), 1.0
+                )
+                self.model.zero_grad()
+                loss.backward()
+                grad = X_adv.grad.data
+                self.model.zero_grad()
+
+                advs_ila[i, :, :, :] = X_adv.data
+
+                # # cur_grad = delta.grad.data
+                # if self.ti_size > 1:  # TI Attack; https://arxiv.org/abs/1904.02884
+                #     self.ti_conv.to(X.device)
+                #     cur_grad = self.ti_conv(cur_grad)
+                #
+                # # MI Attack; https://arxiv.org/abs/1710.06081
+                # cur_grad = normalize_by_pnorm(cur_grad, p=1)
+                # grad = self.momentum * grad + cur_grad
+                if self.ord == np.inf:
+                    X_adv.data += self.step_size * grad.sign()
+                    X_adv.data = clamp(X_adv.data, X - self.epsilon, X + self.epsilon)
+                    X_adv.data = clamp(X_adv, 0.0, 1.0)
+                elif self.ord == 2:
+                    X_adv.data += self.step_size * normalize_by_pnorm(grad, p=2)
+                    X_adv.data *= clamp(
+                        (self.epsilon * normalize_by_pnorm(X_adv.data, p=2) /
+                         X_adv.data),
+                        max=1.)
+                    X_adv.data = clamp(X_adv.data, 0.0, 1.0)
+                else:
+                    error = "Only ord = inf and ord = 2 have been implemented"
+                    raise NotImplementedError(error)
+
+            rval = advs_ila
+
+            del mid_output, mid_original, mid_attack_original
+
+            # X_adv.grad.data.zero_()
+        # rval = advs
         return rval, loss_record
